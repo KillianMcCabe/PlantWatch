47a1f796d3185b28198f6c37704b64ebd7f6d12b
KillianMcCabe <killianparker93@gmail.com>
2019-06-29 00:17:48 +0100

    Added background sprites

--- plant-watch-unity-app/Assets/Scripts/WorldTilter.cs
+++ plant-watch-unity-app/Assets/Scripts/WorldTilter.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WorldTilter : MonoBehaviour
 {
+    float RotateSpeed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,10 @@ public class WorldTilter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Quaternion worldRotation = Quaternion.Euler(0, 0, (Quaternion.Euler(-90f, 0, 0) * Input.gyro.attitude).eulerAngles.z);
+        float angle = (180f - Quaternion.Angle(transform.rotation, worldRotation)) / 180f;
 #if !UNITY_EDITOR
+        // transform.rotation = Quaternion.RotateTowards(transform.rotation, worldRotation, angle * RotateSpeed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(0, 0, (Quaternion.Euler(-90f, 0, 0) * Input.gyro.attitude).eulerAngles.z);
 #endif
     }
