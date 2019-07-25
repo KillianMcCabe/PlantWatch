0981646c2b87d822399f1597373d8c09e15b5bef
KillianMcCabe <killianparker93@gmail.com>
2019-07-25 12:45:56 +0100

    Added bird sprites, prefab and spawning behaviour

---
+++ plant-watch-unity-app/Assets/Scripts/Bird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private const float LifeTime = 8f;
+
+    private Vector3 _flyDirection;
+    private float _flySpeed = 5f;
+    private float _lifeTime = 0f;
+
+    public void Init(Vector3 position, Vector3 flyDirection)
+    {
+        transform.position = position;
+        _flyDirection = flyDirection;
+
+        transform.right = -_flyDirection;
+    }
+
+    private void Update()
+    {
+        transform.position += _flyDirection * _flySpeed * Time.deltaTime;
+
+        _lifeTime += Time.deltaTime;
+        if (_lifeTime > LifeTime)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ plant-watch-unity-app/Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    private const float HorizontalSpawnBoundsPadding = 2;
+    private const float VerticalSpawnBoundsScale = 2;
+
+    [HideInInspector]
+    public float SpawnRate = 5; // how man birds to spawn each second
+
+    [SerializeField]
+    private Bird _birdPrefab;
+
+    private Bounds _spawnBounds;
+
+    private float _timeSinceLastBirdSpawned;
+    private float _timeTilNextBirdIsSpawned;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera camera = Camera.main;
+        float vertCamExtent = camera.orthographicSize;
+        float horzCamExtent = vertCamExtent * Screen.width / Screen.height;
+        _spawnBounds = new Bounds(
+            camera.gameObject.transform.position,
+            new Vector3(
+                (horzCamExtent + HorizontalSpawnBoundsPadding) * 2,
+                (vertCamExtent * VerticalSpawnBoundsScale) * 2,
+                0
+            )
+        );
+
+        _timeSinceLastBirdSpawned = 0;
+        _timeTilNextBirdIsSpawned = SpawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawSpawnBounds();
+
+        _timeSinceLastBirdSpawned += Time.deltaTime;
+        if (_timeSinceLastBirdSpawned > _timeTilNextBirdIsSpawned)
+        {
+            SpawnBird();
+
+            _timeSinceLastBirdSpawned = 0;
+            _timeTilNextBirdIsSpawned = SpawnRate;
+        }
+    }
+
+    private void SpawnBird()
+    {
+        float xPos;
+        Vector3 flyDirection;
+        if (Random.Range(0, 100) > 50)
+        {
+            // bird will fly in from the left side of the screen
+            xPos = _spawnBounds.min.x;
+            flyDirection = Vector3.right;
+        }
+        else
+        {
+            // bird will fly in from the right side of the screen
+            xPos = _spawnBounds.max.x;
+            flyDirection = Vector3.left;
+        }
+
+        float yPos = Random.Range(_spawnBounds.min.y, _spawnBounds.max.y);
+
+        Vector3 spawnPosition = new Vector3(xPos, yPos, 0);
+
+        Bird bird = Instantiate(_birdPrefab);
+        bird.Init(spawnPosition, flyDirection);
+    }
+
+    private void DrawSpawnBounds()
+    {
+        Vector3 bl = new Vector3(_spawnBounds.min.x, _spawnBounds.min.y, 0);
+        Vector3 br = new Vector3(_spawnBounds.max.x, _spawnBounds.min.y, 0);
+        Vector3 tl = new Vector3(_spawnBounds.min.x, _spawnBounds.max.y, 0);
+        Vector3 tr = new Vector3(_spawnBounds.max.x, _spawnBounds.max.y, 0);
+
+        Debug.DrawLine(bl, tl, Color.red);
+        Debug.DrawLine(tl, tr, Color.red);
+        Debug.DrawLine(tr, br, Color.red);
+        Debug.DrawLine(br, bl, Color.red);
+    }
+}
